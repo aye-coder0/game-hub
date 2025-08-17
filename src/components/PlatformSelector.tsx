2786c677628e9b270650480a1a007b6be96c4d9a
aye-coder0 <numeriquelicence@gmail.com>
2025-08-17 23:25:00 +0000

    fix the problem with building the app

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,4 +1,4 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { Button, Menu, MenuButton, MenuItem, MenuList, Icon } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import { Platform } from "../hooks/useGames";
 import usePlatforms from "../hooks/usePlatforms";
@@ -12,10 +12,10 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
 
   if (error) return null;
-  
+
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+      <MenuButton as={Button} rightIcon={<Icon as={BsChevronDown as any} />}>
         {selectedPlatform?.name || 'Platforms'}
       </MenuButton>
       <MenuList>
